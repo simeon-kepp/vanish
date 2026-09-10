57fed435efa2007b52a3b96a73cb0f78d6d148ae
simeon-kepp <rfi.irfos@gmail.com>
2026-09-10 08:34:34 +0200

    VANISH PWA - deploy to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('install', (event) => { self.skipWaiting(); });
+self.addEventListener('activate', (event) => { event.waitUntil(clients.claim()); });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
